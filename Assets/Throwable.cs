b821933e884aebd9993731a7ee72514bac8a9763
DisortingLines <ehorandiclevett@gmail.com>
2021-03-14 15:26:10 +0000

    Throwables now Distract
    Player Move alert in progress

--- Assets/Throwable.cs
+++ Assets/Throwable.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Throwable : MonoBehaviour
 {
     public float throwForce = 10f;
     public Rigidbody rb;
+
+    public float alertRadius = 5f;
 
     void Start()
     {
@@ -16,4 +19,25 @@ public class Throwable : MonoBehaviour
     {
         rb.AddForce(transform.forward * throwForce, ForceMode.Force);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, alertRadius);
+        foreach(var hitCollider in hitColliders)
+        {
+            if(hitCollider.gameObject.tag == "Enemy")
+            {
+                hitCollider.gameObject.GetComponent<NavMeshAgent>().SetDestination(transform.position);
+            }
+        }
+    }
+
+    /*public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawWireSphere(transform.position, alertRadius);
+    }
+    */
+
 }
